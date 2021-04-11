833c1c30488aec62e03b05867f577ab689930fdb
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-04-11 19:47:21 +0430

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -11,7 +11,7 @@ from . import *
 from telegraph import upload_file as uf
 import requests as r
 
-TE = "`OCR_API` not found, Please get it from Ocr.Space and set it in Redis"
+TE = "`API not found, Please get it from ocr.space and set\n\ncommand `{i}setredis OCR_API your-api-key`"
 
 
 @ultroid_cmd(pattern="ocr ?(.*)")
