1561744936d1cdd62d443740e1fa7c955014fa1d
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-31 19:31:51 +0430

    Update fun.py

--- fun.py
+++ fun.py
@@ -13,15 +13,6 @@
 • `{i}decide`
     Decide something.
 
-• `{i}echo <reply to a message>`
-    echo the replied message.
-
-• `{i}gif <your query>`
-    Sends the desired gif related to your query.
-
-• `{i}vtog <in reply to a video>`
-    Converts any video to a gif with low time limit(takes time).
-
 • `{i}xo`
     Opens tic tac game only where using inline mode is allowed.
 
@@ -32,16 +23,12 @@
     Shows the desired place in the map.
 
 """
-
-import os
 import random
-
-import moviepy.editor as m
 import requests
 from bs4 import BeautifulSoup as bs
 from pyjokes import get_joke
+
 from telethon.errors import ChatSendMediaForbiddenError
-from telethon.errors.rpcerrorlist import ChatSendGifsForbiddenError
 
 from . import *
 
@@ -96,40 +83,6 @@ async def _(event):
         await eor(event, r["answer"])
 
 
-@ultroid_cmd(pattern="gif ?(.*)")
-async def gifs(ult):
-    if BOT_MODE:
-        return await eor(ult, "You cant use this Command in BOT MODE")
-    get = ult.pattern_match.group(1)
-    if not get:
-        return await eor(ult, "`.gif <query>`")
-    m = await eor(ult, "`Searching gif ...`")
-    gifs = await ultroid_bot.inline_query("gif", f"{get}")
-    try:
-        await gifs[0].click(
-            ult.chat.id, reply_to=ult.reply_to_msg_id, silent=True, hide_via=True
-        )
-        await m.delete()
-    except ChatSendGifsForbiddenError:
-        await m.edit("`Sending Gif is Restricted in this Chat !!`")
-
-
-@ultroid_cmd(pattern="vtog$")
-async def vtog(ult):
-    reply = await ult.get_reply_message()
-    if reply is None:
-        return await ult.edit("`Reply to any Video`")
-    xx = await eor(ult, "`Processing Takes Time...`")
-    lol = await ultroid_bot.download_media(reply.media)
-    file_name = "cipherx.gif"
-    clip = m.VideoFileClip(lol).subclip((4.3), (5.8)).resize(0.3)
-    clip.write_gif(file_name)
-    await ultroid_bot.send_file(ult.chat_id, file_name, reply_to=ult.reply_to_msg_id)
-    os.remove(lol)
-    os.remove(file_name)
-    await xx.delete()
-
-
 @ultroid_cmd(pattern="xo$")
 async def xo(ult):
     if BOT_MODE:
@@ -139,19 +92,6 @@ async def xo(ult):
         ult.chat.id, reply_to=ult.reply_to_msg_id, silent=True, hide_via=True
     )
     await ult.delete()
-
-
-@ultroid_cmd(pattern="echo")
-async def echoify(e):
-    if not e.is_reply:
-        return await eor(e, "Reply to a Message !")
-    gt = await e.get_reply_message()
-    if gt.text and not gt.media:
-        await eor(e, gt.text)
-    else:
-        await gt.reply(gt)
-        if e.sender_id == ultroid_bot.uid:
-            await e.delete()
 
 
 @ultroid_cmd(pattern="wordi$")
