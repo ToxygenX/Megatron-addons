201b81c634df47fd4e4b19352e95614111d016b3
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-31 19:29:47 +0430

    Update clone.py

--- clone.py
+++ clone.py
@@ -12,7 +12,8 @@
 import html
 
 from telethon.tl.functions.account import UpdateProfileRequest
-from telethon.tl.functions.photos import DeletePhotosRequest, UploadProfilePhotoRequest
+from telethon.tl.functions.photos import (DeletePhotosRequest,
+                                          UploadProfilePhotoRequest)
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 
