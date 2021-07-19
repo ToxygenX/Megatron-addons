163209bb056a4b40e3b9c9d871e23d706ce803a4
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-19 20:34:06 +0430

    Update animedb.py

--- animedb.py
+++ animedb.py
@@ -1,10 +1,10 @@
+# Made by : @Arnab431 || github.com/ArnabXD
+# Made For : https://github.com/TeamUltroid/UltroidAddons
+
 """
 Search animes and manga from anilist.co using @animedb_bot
 
 ✘ Commands Available
-• `{i}anime <keyword>`
-    To get anime info
-
 • `{i}manga <keyword>`
     To get manga info
 """
@@ -13,29 +13,7 @@ from telethon.errors import ChatSendInlineForbiddenError
 
 from . import *
 
-
-@ultroid_cmd(
-    pattern="anime ?(.*)",
-)
-async def anime(ult):
-    msg = await eor(ult, "`Searching ...`")
-    keyword = ult.pattern_match.group(1)
-    if keyword is None:
-        return await msg.edit("`Provide a Keyword to search`")
-    try:
-        animes = await ultroid_bot.inline_query("animedb_bot", keyword)
-        await animes[0].click(
-            ult.chat_id,
-            reply_to=ult.reply_to_msg_id,
-            silent=True if ult.is_reply else False,
-            hide_via=True,
-        )
-        return await msg.delete()
-    except ChatSendInlineForbiddenError:
-        return await msg.edit("`Seems like inline messages aren't allowed here`")
-    except Exception:
-        return await msg.edit("`No Results Found ...`")
-
+INLOCK = "`Seems like inline messages aren't allowed here`"
 
 @ultroid_cmd(
     pattern="manga ?(.*)",
@@ -46,7 +24,7 @@ async def manga(ult):
     if keyword is None:
         return await msg.edit("`Provide a Keyword to search`")
     try:
-        animes = await ultroid_bot.inline_query("animedb_bot", f"<m> {keyword}")
+        animes = await ult.client.inline_query("animedb_bot", f"<m> {keyword}")
         await animes[0].click(
             ult.chat_id,
             reply_to=ult.reply_to_msg_id,
@@ -54,10 +32,5 @@ async def manga(ult):
             hide_via=True,
         )
         return await msg.delete()
-    except ChatSendInlineForbiddenError:
-        return await msg.edit("`Seems like inline messages aren't allowed here`")
     except Exception:
         return await msg.edit("`No Results Found ...`")
-
-
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
