884e5c57ad7793a226dc64dcc16ca9062e320386
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-19 20:40:07 +0430

    Update totalmsgs.py

--- totalmsgs.py
+++ totalmsgs.py
@@ -1,29 +1,26 @@
-# credit https://t.me/I_m_FlaSh
-
 """
 ✘ Commands Available -
-• `{i}totals`
+• `{i}totalmsgs`
     Returns your total messages count in current chat
     
-• `{i}totals [username]/<reply>`
-    Returns total msg count of user in current chat
+• `{i}totalmsgs [username]/<reply>`
+    Returns total messages count of user in current chat
 """
 
 from . import *
 
 
-@ultroid_cmd(pattern="totals ?(.*)")
+@ultroid_cmd(pattern="totalmsgs ?(.*)")
 async def _(e):
-  match = e.pattern_match.group(1)
-  if match:
-    user = match
-  elif e.is_reply:
-    user = (await e.get_reply_message()).sender_id
-  else:
-    user = 'me'
-  a = await ultroid_bot.get_messages(e.chat_id, 0, from_user=user)
-  user = await ultroid_bot.get_entity(user)
-  await eor(e, f"Total messages of `{user.first_name}` here = {a.total}")
+    match = e.pattern_match.group(1)
+    if match:
+        user = match
+    elif e.is_reply:
+        user = (await e.get_reply_message()).sender_id
+    else:
+        user = "me"
+    a = await e.client.get_messages(e.chat_id, 0, from_user=user)
+    user = await e.client.get_entity(user)
+    await eor(e, f"Total messages of `{user.first_name}` here = {a.total}")
 
 
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})  
