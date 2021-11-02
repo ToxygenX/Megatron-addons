aa21a6e64e343c122b5b3c3fcb1921556a3b486e
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-03 02:08:11 +0330

    Update typing.py

--- typing.py
+++ typing.py
@@ -1,12 +1,3 @@
-# (c) Shrimadhav U.K
-# aka Spechide
-#
-# Ported for Ultroid - UserBot
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
@@ -19,33 +10,21 @@ import asyncio
 from . import *
 
 
-@ultroid_cmd(pattern="type ?(.*)")
+@ultroid_cmd(pattern="type ?(.*)", fullsudo=True)
 async def _(event):
     input_str = event.pattern_match.group(1)
     if not input_str:
         return await eor(event, "Give me something to type !")
-    shiiinabot = "\u2060"
-    for i in range(601):
-        shiiinabot += "\u2060"
-    try:
-        okla = await eor(event, shiiinabot)
-    except Exception as e:
-        print(str(e))
+    shiiinabot = "\u2060" * 602
+    okla = await eor(event, shiiinabot)
     typing_symbol = "|"
     previous_text = ""
     await okla.edit(typing_symbol)
-    await asyncio.sleep(0.05)
+    await asyncio.sleep(0.4)
     for character in input_str:
         previous_text = previous_text + "" + character
         typing_text = previous_text + "" + typing_symbol
-        try:
-            await okla.edit(typing_text)
-        except Exception as e:
-            print(str(e))
-        await asyncio.sleep(0.05)
-        try:
-            await okla.edit(previous_text)
-        except Exception as e:
-            print(e)
-        await asyncio.sleep(0.05)
-
+        await okla.edit(typing_text)
+        await asyncio.sleep(0.4)
+        await okla.edit(previous_text)
+        await asyncio.sleep(0.4)
