b70987969effbf5db41b66710ed01b04cf1c6383
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-03 01:55:14 +0330

    Update quotefancy.py

--- quotefancy.py
+++ quotefancy.py
@@ -1,11 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
-
 """
 ✘ Commands Available
 
@@ -13,9 +5,8 @@
     Gets random quotes from QuoteFancy.com.
 """
 
-from telethon.errors import ChatSendMediaForbiddenError
-
 from quotefancy import get_quote
+from telethon.errors import ChatSendMediaForbiddenError
 
 from . import *
 
@@ -25,13 +16,11 @@ async def quotefancy(e):
     mes = await eor(e, "`Processing...`")
     img = get_quote("img", download=True)
     try:
-        await ultroid_bot.send_file(e.chat_id, img)
+        await e.client.send_file(e.chat_id, img)
         os.remove(img)
         await mes.delete()
     except ChatSendMediaForbiddenError:
         quote = get_quote("text")
         await eor(e, f"`{quote}`")
     except Exception as e:
-        await eor(e, f"**ⲈRRⲞR** - {str(e)}")
-
-
+        await eor(e, f"**ERROR** - {str(e)}")
