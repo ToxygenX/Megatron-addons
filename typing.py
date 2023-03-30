3500f13c680282a1dae49bd3d899bfe2f149d652
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-30 14:45:08 +0330

    Update typing.py

--- typing.py
+++ typing.py
@@ -14,17 +14,17 @@ from . import *
 async def _(event):
     input_str = event.pattern_match.group(1)
     if not input_str:
-        return await eor(event, "Give me something to type !")
+        return await event.eor("Give me something to type !")
     shiiinabot = "\u2060" * 602
-    okla = await eor(event, shiiinabot)
+    okla = await event.eor(shiiinabot)
     typing_symbol = "|"
     previous_text = ""
     await okla.edit(typing_symbol)
-    await asyncio.sleep(0.05)
+    await asyncio.sleep(0.4)
     for character in input_str:
         previous_text = previous_text + "" + character
         typing_text = previous_text + "" + typing_symbol
         await okla.edit(typing_text)
-        await asyncio.sleep(0.05)
+        await asyncio.sleep(0.4)
         await okla.edit(previous_text)
-        await asyncio.sleep(0.05)
+        await asyncio.sleep(0.4)
