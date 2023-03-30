8ee10ae06afa59573f4c14ec515b5ab5dbeba2f0
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-30 14:24:32 +0330

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -3,41 +3,37 @@
 
 • `{i}ocr <language code><reply to a photo>`
     text recognition service.
-
 """
 
 
-import requests as r
 from telegraph import upload_file as uf
 
 from . import *
 
-TE = f"API not found, Please get it from ocr.space and set\n\ncommand `{HNDLR}setredis OCR_API your-api-key`"
+TE = f"API not found, Please get it from ocr.space and set\n\ncommand `{HNDLR}setdb OCR_API your-api-key`"
 
 
 @ultroid_cmd(pattern="ocr ?(.*)")
 async def ocrify(ult):
     if not ult.is_reply:
-        return await eor(ult, "`Reply to Photo...`")
-    msg = await eor(ult, "`Processing...`")
-    OAPI = udB.get("OCR_API")
+        return await ult.eor("`Reply to Photo...`")
+    msg = await ult.eor("`Processing..`")
+    OAPI = udB.get_key("OCR_API")
     if not OAPI:
         return await msg.edit(TE)
     pat = ult.pattern_match.group(1)
     repm = await ult.get_reply_message()
     if not (repm.media and repm.media.photo):
-        return await msg.edit("`Not a Photo...`")
+        return await msg.edit("`Not a Photo..`")
     dl = await repm.download_media()
+    atr = ""
     if pat:
-        atr = f"&language={pat}&"
-    else:
-        atr = "&"
+        atr = f"&language={pat}"
     tt = uf(dl)
     li = "https://telegra.ph" + tt[0]
-    gr = r.get(
-        f"https://api.ocr.space/parse/imageurl?apikey={OAPI}{atr}url={li}"
-    ).json()
+    gr = await async_searcher(
+        f"https://api.ocr.space/parse/imageurl?apikey={OAPI}{atr}&url={li}",
+        re_json=True,
+    )
     trt = gr["ParsedResults"][0]["ParsedText"]
-    await msg.edit(f"**🎇 ⲞⲤR Ⲣⲟʀⲧⲁⳑ\n\\Rⲉⲋυⳑⲧⲋ ~ ** `{trt}`")
-
-
+    await msg.edit(f"**🎉 OCR PORTAL\n\nRESULTS ~ ** `{trt}`")
