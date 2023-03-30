0db39783a51ce0f5f416aef9f609238e4135da1d
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-30 14:23:29 +0330

    Update ncode.py

--- ncode.py
+++ ncode.py
@@ -1,49 +1,46 @@
-# Credits @buddhhu
-# Ported to Ultroid by @Hackintush
-
 """
-✘ Commands Available
-• `{i}ncode <reply to file>`
-   Paste the contents of file and send as pic.
-"""
+✘ Commands Available -
 
-import os
+• `{i}ncode <file>`
+   Use - Paste the contents of file and send as pic.
+"""
 
 import pygments
 from pygments.formatters import ImageFormatter
 from pygments.lexers import Python3Lexer
+from . import ultroid_cmd, check_filename
 
-from . import *
 
-
-@ultroid_cmd(pattern="ncode")
+@ultroid_cmd(pattern="ncode$")
 async def coder_print(event):
-    a = await event.client.download_media(
-        await event.get_reply_message(),
-    )
-    s = open(a, "r")
-    c = s.read()
-    s.close()
+    if not event.reply_to_msg_id:
+        return await event.eor("`Reply to a file or message!`", time=5)
+    msg = await event.get_reply_message()
+    if msg.document:
+        a = await event.client.download_media(
+            await event.get_reply_message(), check_filename("ncode.png")
+        )
+        with open(a, "r") as s:
+            c = s.read()
+    else:
+        a = None
+        c = msg.text
     pygments.highlight(
-        f"{c}",
+        c,
         Python3Lexer(),
-        ImageFormatter(font_name="DejaVu Sans Mono", line_numbers=True),
-        "cipherx.png",
+        ImageFormatter(line_numbers=True),
+        check_filename("result.png"),
     )
     res = await event.client.send_message(
         event.chat_id,
-        "Pasting this code on my page...",
+        "**Pasting this code on my page...**",
         reply_to=event.reply_to_msg_id,
     )
     await event.client.send_file(
-        event.chat_id,
-        "cipherx.png",
-        force_document=True,
-        reply_to=event.reply_to_msg_id,
+        event.chat_id, "result.png", force_document=True, reply_to=event.reply_to_msg_id
     )
     await res.delete()
     await event.delete()
-    os.remove(a)
-    os.remove("cipherx.png")
-
-
+    if a:
+        os.remove(a)
+    os.remove("result.png")
