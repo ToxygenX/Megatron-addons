01dddb62e2948e31002780c2138463b8a360dcbd
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-30 13:41:38 +0330

    Update covid.py

--- covid.py
+++ covid.py
@@ -1,23 +1,23 @@
 """
 ✘ Commands Available
+
 • `{i}covid country name`
     Gets the Covid-19 Status of a given Country.
 """
 
 from covid import Covid
 
-from . import *
+from . import ultroid_cmd
 
 
 @ultroid_cmd(pattern="covid")
 async def coronish(event):
     covid = Covid()
-    text = event.text
-    okie = text.split(" ", maxsplit=1)
+    okie = event.text.split(maxsplit=1)
     try:
         country = okie[1]
     except IndexError:
-        await eor(event, "Give a country name to Search for it's Covid Cases!")
+        await event.eor("Give a country name to Search for it's Covid Cases!")
         return
     try:
         cases = covid.get_status_by_country_name((country).lower())
@@ -25,9 +25,8 @@ async def coronish(event):
         conf = cases["confirmed"]
         dec = cases["deaths"]
         rec = cases["recovered"]
-        await eor(
-            event,
+        await event.eor(
             f"**Country:** **{country.capitalize()}**\n**Active:** {act}\n**Confirmed:** {conf}\n**Recovered:** {rec}\n**Deceased:** {dec}",
         )
     except ValueError:
-        await eor(event, f"It seems that Country {country} is invalid!")
+        await event.eor(f"It seems that Country {country} is invalid!")
