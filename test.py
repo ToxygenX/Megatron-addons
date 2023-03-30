d0b6e021d4944d892c65cbd8e82bec2990753072
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-30 14:39:50 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
-# Ported From DarkCobra Originally By UNIBORG
-#
-# Ultroid - UserBot
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
 • `{i}test`
-    Test CɪᴘʜᴇʀX Server Speed.
+    Test Your Server Speed.
 
 """
+
 from datetime import datetime
 
 import speedtest
@@ -22,23 +15,18 @@ from . import *
 
 @ultroid_cmd(pattern="test ?(.*)")
 async def _(event):
-    if event.fwd_from:
-        return
     input_str = event.pattern_match.group(1)
-    as_text = True
-    as_document = False
+    as_document = None
     if input_str == "image":
         as_document = False
     elif input_str == "file":
         as_document = True
-    elif input_str == "text":
-        as_text = True
-    xx = await eor(event, "`Ⲥⲁⳑⲥυⳑⲁⲧⲓⲛⳋ CɪᴘʜᴇʀX Ⲋⲉʀⳳⲉʀ Ⲋⲣⲉⲉⲇ...`")
+    xx = await event.eor("`Calculating ur Ultroid Server Speed. Please wait!`")
     start = datetime.now()
     s = speedtest.Speedtest()
     s.get_best_server()
     s.download()
-    s.upload()  # dchehe
+    s.upload()
     end = datetime.now()
     ms = (end - start).seconds
     response = s.results.dict()
@@ -51,21 +39,21 @@ async def _(event):
     reply_msg_id = event.message.id
     if event.reply_to_msg_id:
         reply_msg_id = event.reply_to_msg_id
-    try:  # heheh
+    try:
         response = s.results.share()
         speedtest_image = response
-        if as_text:
+        if as_document is None:
             await xx.edit(
-                """`CɪᴘʜᴇʀX Ⲋⲉʀⳳⲉʀ Ⲥⲁⳑⲥυⳑⲁⲧⲉⲇ Ⲋⲣⲉⲉⲇ Ⲓⲛ {} Ⲋⲉⲥ`
+                """`Ultroid Server Speed in {} sec`
 
-`Dᴏwnlᴏᴀd: {}`
-`Uᴩlᴏᴀd: {}`
+`Download: {}`
+`Upload: {}`
 `Ping: {}`
-`Inᴛᴇrnᴇᴛ Sᴇrviᴄᴇ Prᴏvidᴇr: {}`
-`ISP Rᴀᴛing: {}`""".format(
+`Internet Service Provider: {}`
+`ISP Rating: {}`""".format(
                     ms,
-                    convert_from_bytes(download_speed),
-                    convert_from_bytes(upload_speed),
+                    humanbytes(download_speed),
+                    humanbytes(upload_speed),
                     ping_time,
                     i_s_p,
                     i_s_p_rating,
@@ -74,8 +62,8 @@ async def _(event):
         else:
             await event.client.send_file(
                 event.chat_id,
-                speedtest_image,  # heeehe
-                caption="**Ⲋⲣⲉⲉⲇ Ⲧⲉⲋⲧ** Ⲥⲟⲙⲣⳑⲉⲧⲉⲇ ⲓⲛ {} Ⲋⲉⲥⲟⲛⲇⲋ".format(ms),
+                speedtest_image,
+                caption="**SpeedTest** completed in {} seconds".format(ms),
                 force_document=as_document,
                 reply_to=reply_msg_id,
                 allow_cache=False,
@@ -83,7 +71,7 @@ async def _(event):
             await event.delete()
     except Exception as exc:  # dc
         await xx.edit(
-            """**Ⲋⲣⲉⲉⲇ Ⲧⲉⲋⲧ** Ⲥⲟⲙⲣⳑⲉⲧⲉⲇ ⲓⲛ {} Ⲋⲉⲥⲟⲛⲇⲋ
+            """**SpeedTest** completed in {} seconds
 Download: {}
 Upload: {}
 Ping: {}
@@ -92,20 +80,9 @@ Ping: {}
 __With the Following ERRORs__
 {}""".format(
                 ms,
-                convert_from_bytes(download_speed),
-                convert_from_bytes(upload_speed),
+                humanbytes(download_speed),
+                humanbytes(upload_speed),
                 ping_time,
                 str(exc),
             )
         )
-
-
-def convert_from_bytes(size):
-    power = 2 ** 10
-    n = 0
-    units = {0: "", 1: "kilobytes", 2: "megabytes", 3: "gigabytes", 4: "terabytes"}
-    while size > power:
-        size /= power
-        n += 1
-    return f"{round(size, 2)} {units[n]}"
-
